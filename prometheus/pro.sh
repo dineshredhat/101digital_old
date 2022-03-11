7b99f586ff12712e1399633ca364c8b958e209b4
dineshredhat <dinesh.redhat@yahoo.com>
2022-03-11 10:22:33 +0530

    Delete prometheus directory

--- prometheus/pro.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace monitoring
-kubectl apply -f clusterRole.yaml
-kubectl apply -f config-map.yaml
-kubectl apply  -f prometheus-deployment.yaml 
-kubectl apply -f prometheus-service.yaml --namespace=monitoring
