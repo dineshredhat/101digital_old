d5507a35a6c0e82993d798ac38b24a7ad890d524
dineshredhat <dinesh.redhat@yahoo.com>
2022-03-11 10:22:48 +0530

    Delete terraform directory

--- terraform/internet_gateway.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "101digital" {
-    vpc_id = "${aws_vpc.101digital.id}"
-    tags {
-        Name = "101digital"
-    }
-}
--- terraform/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "101digital" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = true
-    enable_dns_hostnames = true    
-    tags {
-        Name = "101digital"
-    }
-}
